afc480dca746625ce2231c2b93b9f5c6f972a611
S M Farhan Samir <samir@S-M-Farhan-Samirs-MacBook-Pro.local>
2024-08-28 22:15:08 -0700

    update constants

--- packages/constants.py
+++ packages/constants.py
@@ -1,7 +1,9 @@
 SCRATCH_DIR = f"scratch"
 MY_CACHE_DIR = "my_cache" # as opposed to agent cache dir.
 LEAP_CACHE_DIR ="leap_cache"
+INDIVIDUAL_QUERY_DIR = "individual_query_cache"
 MY_LOG_DIR = "my_logs"
+INDIVIDUAL_Q_LOGS = "individual_query_logs"
 EMPTY_SC_ANSWER = "samples only"
 
 DB_ANALYSIS_FAILED = "The experiment failed, so the claim cannot be evaluated."
